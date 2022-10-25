ae31a660b9a8645656b4c8ca522d27c5f5699f70
dy6578ekdbs <dy6578ekdbs@gmail.com>
2022-10-25 18:02:05 +0900

    project setting

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles/reset.scss";
+import "./styles/font.scss";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Test from "./components/Test";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/*" element={<Test />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Test.js
@@ -0,0 +1,12 @@
+import "../styles/font.scss";
+import styled from "styled-components";
+
+const Test = () => {
+  return <Style>Test</Style>;
+};
+
+export default Test;
+
+const Style = styled.div`
+  color: var(--main-pink);
+`;
