824409012991dab96a686c7c5d8386cc984dd8b4
dy6578ekdbs <dy6578ekdbs@gmail.com>
2022-11-29 17:35:39 +0900

    feat : GoBackBar 컴포넌트이동 path 추가

--- src/components/Navigate/GoBackBar.jsx
+++ src/components/Navigate/GoBackBar.jsx
@@ -1,17 +1,24 @@
 import { B } from './Nav.style';
 import Back from '../../assets/Navigate/Back.svg';
 
-const GoBackBar = ({ TopBarName, center }) => {
+import { useNavigate } from 'react-router-dom';
+const GoBackBar = ({ TopBarName, center, path }) => {
+  const navigate = useNavigate();
+
+  const Goto = () => {
+    navigate(path);
+    console.log('이동');
+  };
   return (
     <B.Rectangle>
       {center ? (
         <>
-          <B.Back src={Back} center />
+          <B.Back src={Back} center onClick={Goto} />
           <B.TopBarName center>{TopBarName}</B.TopBarName>
         </>
       ) : (
         <>
-          <B.Back src={Back} />
+          <B.Back src={Back} onClick={Goto} />
           <B.TopBarName>{TopBarName}</B.TopBarName>
         </>
       )}
