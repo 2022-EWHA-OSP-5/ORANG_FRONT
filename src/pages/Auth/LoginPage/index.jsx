43deb718d128caa0d284c4eda6eb92fcf5e8e18f
dy6578ekdbs <dy6578ekdbs@gmail.com>
2022-11-29 17:36:35 +0900

    feat : 로그인 api 연결 (미완)

--- src/pages/Auth/LoginPage/index.jsx
+++ src/pages/Auth/LoginPage/index.jsx
@@ -3,21 +3,50 @@ import GoBackBar from '../../../components/Navigate/GoBackBar';
 import Title_Logo from '../../../assets/Logo/Title_Logo.svg';
 import AuthInput from '../../../components/Input/AuthInput';
 import OrangeBtn from '../../../components/Button/OrangeBtn';
+import { useState } from 'react';
+import axios from 'axios';
+
 export default function LoginPage() {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const Login = () => {
+    axios
+      .post('http~', {
+        username: id,
+        password: password,
+      })
+      .then(res => {
+        console.log(res.message);
+        var userData = res.data;
+        localStorage.setItem('user', userData);
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <Layout.Display>
-      <GoBackBar TopBarName="로그인" />
+      <GoBackBar TopBarName="로그인" path="/" />
+
       <Com.Logo src={Title_Logo} />
       <Layout.InputBox>
         <Layout.Input>
-          <AuthInput InputType="아이디" />
+          <AuthInput
+            InputType="아이디"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
         </Layout.Input>
         <Layout.Input>
-          <AuthInput InputType="비밀번호" />
+          <AuthInput
+            InputType="비밀번호"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
         </Layout.Input>
       </Layout.InputBox>
 
-      <OrangeBtn>로그인</OrangeBtn>
+      <OrangeBtn onClick={() => Login()}>로그인</OrangeBtn>
 
       <Com.Text>
         회원이 아니신가요? <span>SIGN UP</span>
