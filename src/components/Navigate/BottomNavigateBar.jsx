f42e3418dc52825d32f35e85e3099dba454d32e7
dy6578ekdbs <dy6578ekdbs@gmail.com>
2022-12-02 23:11:36 +0900

    feat : 하단바 path 추가

--- src/components/Navigate/BottomNavigateBar.jsx
+++ src/components/Navigate/BottomNavigateBar.jsx
@@ -2,14 +2,18 @@ import { Bottom } from './Nav.style';
 import Home from '../../assets/Navigate/Home.svg';
 import Person from '../../assets/Navigate/Person.svg';
 import Plus from '../../assets/Navigate/Plus.svg';
+
+import { useNavigate } from 'react-router-dom';
 const BottomNavigateBar = () => {
+  const navigate = useNavigate();
+
   return (
     <Bottom.Rectangle>
-      <Bottom.Home src={Home} />
+      <Bottom.Home src={Home} onClick={() => navigate('/')} />
 
-      <Bottom.Circle src={Plus} />
+      <Bottom.Circle src={Plus} onClick={() => navigate('/add')} />
 
-      <Bottom.Person src={Person} />
+      <Bottom.Person src={Person} onClick={() => navigate('/mypage')} />
     </Bottom.Rectangle>
   );
 };
