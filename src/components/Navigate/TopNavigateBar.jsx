a30f084be54dcc5c9c00101b06872a69bddcdb6b
dy6578ekdbs <dy6578ekdbs@gmail.com>
2022-12-12 00:57:50 +0900

    feat : top네비게이션 바 path 추가

--- src/components/Navigate/TopNavigateBar.jsx
+++ src/components/Navigate/TopNavigateBar.jsx
@@ -2,13 +2,14 @@ import { N } from './Nav.style';
 import BookMark from '../../assets/Navigate/BookMark.svg';
 import Logo from '../../assets/Navigate/Logo.svg';
 import Orang from '../../assets/Navigate/Orang.svg';
-
+import { useNavigate } from 'react-router-dom';
 const TopNavigateBar = () => {
+  const navigate = useNavigate();
   return (
     <N.Rectangle>
-      <N.Logo src={Logo} />
-      <N.Orang src={Orang} />
-      <N.BookMark src={BookMark} />
+      <N.Logo src={Logo} onClick={() => navigate('/')} />
+      <N.Orang src={Orang} onClick={() => navigate('/')} />
+      <N.BookMark src={BookMark} onClick={() => navigate('/mypage')} />
     </N.Rectangle>
   );
 };
